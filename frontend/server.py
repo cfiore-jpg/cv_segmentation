77c24503ea0681f13838be2636e614ab023af470
Marth4977 <77819310+Marth4977@users.noreply.github.com>
2021-12-11 15:53:35 -0500

    reformatting and processing
    finished going from segmentation to having parameters necessary to recieve secondary inputs

--- frontend/server.py
+++ frontend/server.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
-#from cv_segmentation.layerReplacement.layer_replacement import *
+
+# from cv_segmentation.layerReplacement.layer_replacement import *
 
 
 UPLOAD_FOLDER = os.getcwd()
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'} #additional extensions, .mp4, .mov, .tiff, .avi
-
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}  # additional extensions, .mp4, .mov, .tiff, .avi
 
 
 # Takes in a filename and verifies whether it is in the bounds of the allowed
@@ -16,6 +16,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'} #additional ext
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # def download_file(name):
 #     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
@@ -35,8 +36,8 @@ def upload_file():
         if not have_file and not have_secondary:
             flash('No file part')
             return redirect(request.url)
-        
-        #handles primary input uploading
+
+        # handles primary input uploading
         if have_file:
             if file.filename == '':
                 flash('No selected file')
@@ -45,10 +46,10 @@ def upload_file():
                 filename = secure_filename(file.filename)
                 new_path = os.path.join(app.config['UPLOAD_FOLDER'], "static/data/primary_inputs", filename)
                 file.save(new_path)
-                #url_path = os.path.join("data/uploads", filename)
-                #return redirect(url_for('download_file', name=url_path))
+                # url_path = os.path.join("data/uploads", filename)
+                # return redirect(url_for('download_file', name=url_path))
                 return "File uploaded to static/data/primary_inputs"
-        #handles secondary input uploading
+        # handles secondary input uploading
         if have_secondary:
             if secondary_input.filename == '':
                 flash('No selected file')
@@ -57,31 +58,31 @@ def upload_file():
                 filename = secure_filename(secondary_input.filename)
                 new_path = os.path.join(app.config['UPLOAD_FOLDER'], "static/data/secondary_inputs", filename)
                 secondary_input.save(new_path)
-                #url_path = os.path.join("data/uploads", filename)
-                #return redirect(url_for('download_file', name=url_path))
+                # url_path = os.path.join("data/uploads", filename)
+                # return redirect(url_for('download_file', name=url_path))
                 return "File uploaded to static/data/secondary_inputs"
     return
+
 
 def display_list(list):
     return list
 
+
 def replace_layers(list):
     return list
+
 
 @app.route('/', methods=['GET', 'POST'])
 # Runs the upload method and returns the rendered page
 def index():
     upload_return = upload_file()
-    list = display_list(["car", "dog", "tree", "house", "sidewalk", "bike", "crosswalk", "pedestrian", "garage", "bush", "lawn"])
+    list = display_list(
+        ["car", "dog", "tree", "house", "sidewalk", "bike", "crosswalk", "pedestrian", "garage", "bush", "lawn"])
     return render_template('index.html', upload_return=upload_return, list_len=len(list), list=list)
 
-#2.) Displaying list of layers with options for secondary input
+# 2.) Displaying list of layers with options for secondary input
 
-#need to figure out how to link images in the secondary_inputs folder with the layer
-#they need to replace
+# need to figure out how to link images in the secondary_inputs folder with the layer
+# they need to replace
 
-#add segment button, write todo w/ empty function
-
-
-
-
+# add segment button, write todo w/ empty function
--- layerReplacement/processing.py
+++ layerReplacement/processing.py
@@ -1,76 +1,76 @@
 import numpy as np
 import pims
 
-def make_layer_matrices(semantic_output):
 
-    '''
+def make_layer_matrices(semantic_output):
+    """
     Description:
-    Turns output from semantic segementation into an input usable for layer 
+    Turns output from semantic segmentation into an input usable for layer
     replacement.
 
     Inputs:
-        semantic_output: an m x n np matrix array that contains layer numbers 
+        semantic_output: an f x m x n np matrix array that contains layer numbers for all frames
 
     Outputs:
-        layer_matrices: a 3 dimensional l x m x n np array that contains 1s and 0s
+        layer_matrices: a 4 dimensional l x f x m x n np array that contains 1s and 0s
 
         unique_layers: a 1 dimensional np array of size l with label numbers that correspond
-        to each layer matrix 
+        to each layer matrix
+    """
 
-
-    '''
-    
     unique_layers = np.unique(semantic_output)
     amount_layers = unique_layers.shape[0]
 
-    zerosArray = np.zeros(semantic_output.shape)
-    onesArray = np.ones(semantic_output.shape)
+    zeros_array = np.zeros(semantic_output.shape)
+    ones_array = np.ones(semantic_output.shape)
 
-    layer_matrices = np.empty((amount_layers, semantic_output.shape[0], semantic_output.shape[1]))
-    
-    for index, layer_number in enumerate(unique_layers):
-        layer_matrix = np.where((semantic_output == layer_number),
-        onesArray,
-        zerosArray)
+    layer_matrices = np.empty((amount_layers, semantic_output.shape[0], semantic_output.shape[1],
+                               semantic_output.shape[2]))
 
-        layer_matrices[index] = layer_matrix
+    for layer_index, layer_number in enumerate(unique_layers):
+        for frame_index, frame in enumerate(semantic_output):
+            
+            layer_matrix = np.where((frame == layer_number),
+                                    ones_array,
+                                    zeros_array)
 
-    
+            layer_matrices[layer_index][frame_index] = layer_matrix
+
     return layer_matrices, unique_layers
-        #TODO: add layer matrix to larger return value then return after loop
+    # TODO: add layer matrix to larger return value then return after loop
 
 
 def open_file(file_path):
-
-    '''
+    """
     Description:
-    first method called after pressing 'segment' in front-end
+    first method called after pressing 'segment' in front-end. SEGMENTATION OCCURS HERE
 
     Inputs:
         file_path: string detailing path of file.
         Extension already approved
 
     Outputs:
-    an f x m x n array
-    '''
+    an f x m x n np array containing segmentation output.
+    """
 
     images = pims.open(file_path)
-    #TODO: Verify that its readable and send ERROR if its not
+    # TODO: Verify that its readable and send ERROR if its not
 
     outputArray = np.empty((images.shape[:2]))
 
     for frame in images:
-
         x = 1
-        #perform semantic segmentation, give outputed layer to outputArray
+        # perform semantic segmentation, give outputed layer to outputArray
 
     return outputArray
 
 
 def test_pims(filepath):
-    #do things here to test out
+    # do things here to test out
     print("commencing testing")
 
     images = pims.open(filepath)
 
     print(images[0].shape)
+
+# TODO: incorporate a pims-pipeline method
