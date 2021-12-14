71a1db5be4d84686a89cb00739ec4c97a86c7c5c
cfiore-jpg <77872473+cfiore-jpg@users.noreply.github.com>
2021-12-14 16:56:25 -0500

    Add files via upload

---
+++ layerReplacement/read_labels.py
@@ -0,0 +1,12 @@
+import json
+
+def read_labels(dict_path):
+    f = open(dict_path, "r")
+    l = json.load(f)
+    labels = {}
+    for key in l:
+        labels[int(key)] = l[key]
+    return labels
+
+labels = read_labels("labels.json")
+print(labels)
